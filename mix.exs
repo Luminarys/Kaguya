fb873fee50dbb61390816282fc0f71895cdffe49
Luminarys <kizunanohikari@gmail.com>
2016-01-08 22:38:46 -0600

    Increment version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Kaguya.Mixfile do
 
   def project do
     [app: :kaguya,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
