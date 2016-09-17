28341b2610a1d9a11e0bbf17bfaed076bd4aa17d
Luminarys <postmaster@gensok.io>
2016-09-17 01:10:02 -0500

    Increment minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kaguya.Mixfile do
   def project do
     [
       app: :kaguya,
-      version: "0.4.8",
+      version: "0.4.9",
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
